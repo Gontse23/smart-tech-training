8f7183e814100b7f8cb7fd94cdbc88da8fb3b054
Gontse Mfete <gontsemfete@gmail.com>
2026-04-30 22:50:52 +0200

    Update API proxy for production

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      "/api": "http://localhost:4000"
+      "/api": "https://smart-tech-training-api.onrender.com"
     }
   },
   build: {
